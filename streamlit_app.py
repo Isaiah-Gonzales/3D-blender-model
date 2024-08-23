64af3ab7a44c4de6b6729aeb9509f5a85e068ac0
Isaiah Gonzales <110946128+Isaiah-Gonzales@users.noreply.github.com>
2024-08-23 16:06:48 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -113,9 +113,16 @@ with tab1:
       
       figure, ax = plt.subplots(figsize=(10,10))
       viz = ax.boxplot(meanResults)
-      plt.title("Spread of mean assays for simulated blender with distribution = " + str(distribution))
+      plt.title("Spread of mean assays for simulated blender with distribution = " + str(distribution) + " y axis fit to data")
       plt.ylabel("Mean Assay (%)")
       st.pyplot(figure)
+
+      figure2, ax2 = plt.subplots(figsize=(10,10))
+      viz = ax2.boxplot(meanResults)
+      plt.title("Spread of mean assays for simulated blender with distribution = " + str(distribution) + " standardized y-axes (so you can compare plots more easily)")
+      plt.ylim((0,150))
+      plt.ylabel("Mean Assay (%)")
+      st.pyplot(figure2)
   
       #show results table
       metrics = ["Min. Average Assay Observed (%)", 
