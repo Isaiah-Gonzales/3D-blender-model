c5f24a4df9a9703e1e94924d7a3bc7885eee94e4
Isaiah Gonzales <110946128+Isaiah-Gonzales@users.noreply.github.com>
2024-08-23 10:26:16 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -26,7 +26,7 @@ with tab1:
     if distribution == "poor":
       with advanced_options:
         percentClumps = st.slider("What percent of DS particles would you like clumped?", min_value = 1,value=50, step =10)
-        sizeClumps = st.number_input("Size of DS clumps (microns)", min_value = 100,value=1000, max_value =10000)
+        sizeClumps = st.number_input("Size of DS clumps (microns)", min_value = particleSize,value=1000, max_value =10000)
     
     if model_type == "multiple blenders":
       numLoops = int(st.sidebar.number_input("How many simulations would you like to perform and average", min_value = 1, max_value = 500))
